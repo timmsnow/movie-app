4a2ca89f07b60f9cee71b12e28288eda67a1a6a5
TIMM SNOW <two.tone.snow@gmail.com>
2021-02-25 18:05:04 -0500

    adds Movie model, creates an instance of Movie

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,6 @@
 # actor.save
 
 # actor = Actor.create({ first_name: "Colin", last_name: "Farrel", known_for: "In Bruges" })
-actor = Actor.create({ first_name: "Kermit", last_name: "the Frog", known_for: "Muppet Babies" })
+# actor = Actor.create({ first_name: "Kermit", last_name: "the Frog", known_for: "Muppet Babies" })
+
+movie = Movie.create({ title: "Priscilla, Queen of the Desert", year: 1994, plot: "Three drag queens from Sydney drive on an epic road trip across the Australian outback to get out of the city and perform a residency in Alice Springs... along the way hilarity ensues." })
