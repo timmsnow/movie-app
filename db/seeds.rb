f6e0e9a7c47d9bb6c41f5444950b760be3107398
TIMM SNOW <two.tone.snow@gmail.com>
2021-02-25 18:14:05 -0500

    generates controller for movie model

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,8 @@
 # actor = Actor.create({ first_name: "Colin", last_name: "Farrel", known_for: "In Bruges" })
 # actor = Actor.create({ first_name: "Kermit", last_name: "the Frog", known_for: "Muppet Babies" })
 
-movie = Movie.create({ title: "Priscilla, Queen of the Desert", year: 1994, plot: "Three drag queens from Sydney drive on an epic road trip across the Australian outback to get out of the city and perform a residency in Alice Springs... along the way hilarity ensues." })
+# movie = Movie.create({ title: "Priscilla, Queen of the Desert", year: 1994, plot: "Three drag queens from Sydney drive on an epic road trip across the Australian outback to get out of the city and perform a residency in Alice Springs... along the way hilarity ensues." })
+# movie = Movie.create({ title: "Run Lola Run", year: 1998, plot: "Fiery, red-haired Lola has only 20 minutes to rescue her boyfriend after a botched deal. The entire city of Berlin plays host to her desperate race against the clock." })
+# movie = Movie.create({ title: "Atonement", year: 2007, plot: "Two sisters find themselves embittered after a bit of hearsay unravels a tightly knit family in WWII era England." })
+# movie = Movie.create({ title: "Eternal Sunshine of the Spotless Mind", year: 2004, plot: "When their relationship turns sour, a couple undergoes a medical procedure to have each other erased from their memories." })
+# movie = Movie.create({ title: "Muriels Wedding", year: 1994, plot: "Socially inept Muriel navigates adulthood, a strong appreciation for ABBA, and an all-consuming desire to get married." })
