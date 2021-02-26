ad73211c6c5be76df523fc58274d6956463247f6
TIMM SNOW <two.tone.snow@gmail.com>
2021-02-26 13:09:19 -0500

    puts all routes in one namespace

--- config/routes.rb
+++ config/routes.rb
@@ -5,21 +5,8 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/actor_path" => "actors#actor_action"
-  end
-
-  namespace :api do
     get "/colin_path" => "actors#colin_action"
-  end
-
-  namespace :api do
     get "/kermit_path" => "actors#kermit_action"
-  end
-
-  namespace :api do
+    get "/one_movie_path" => "movies#one_movie_action"
     get "/all_movies_path" => "movies#all_movies_action"
   end
-
-  namespace :api do
-    get "/one_movie_path" => "movies#one_movie_action"
-  end
-end
