5caca7baabc1713a18adc53822e2476b8fba31be
TIMM SNOW <two.tone.snow@gmail.com>
2021-03-11 18:15:49 -0500

    creates admin authentication for creating, updating and destroying actors and movies

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,15 @@ Rails.application.routes.draw do
     get "/movies/:id" => "movies#show"
     patch "/movies/:id" => "movies#update"
     delete "/movies/:id" => "movies#destroy"
+
     get "/actors" => "actors#index"
     post "/actors" => "actors#create"
     get "/actors/:id" => "actors#show"
     patch "/actors/:id" => "actors#update"
     delete "/actors/:id" => "actors#destroy"
+
+    post "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
   end
 end
