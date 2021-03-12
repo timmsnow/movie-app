02ba3fa1be6edf2f36402c7a849e1a5344004920
TIMM SNOW <two.tone.snow@gmail.com>
2021-03-12 18:05:28 -0500

    adds join table of MovieGenre, makes association and adds method to movie model to display only genre names

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,11 @@
 class Movie < ApplicationRecord
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
+  def genre_names
+    genres.map do |genre|
+      genre.name
+    end
+  end
 end
